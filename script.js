f5474b129a9389fe6c51e141dd1200e603d4c88f
Roeh Lewit <69601986+roehlewit@users.noreply.github.com>
2020-10-22 07:44:03 -0400

    Completed

---
+++ script.js
@@ -0,0 +1,18 @@
+const toggle = document.getElementById('toggle');
+console.log(toggle);
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+
+//Toggle navigation
+toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'));
+
+// show modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+//hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+//hode modal on outside click
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
